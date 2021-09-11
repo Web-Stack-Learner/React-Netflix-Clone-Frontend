4af7948584699c8977b0d088173fbfde08b3cb1c
Web-Stack-Learner <borhan.dev@gmail.com>
2021-09-11 11:21:04 +0600

    Finalize React Frontend Clone

---
+++ src/page/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Featured from '../shared/components/Featured'
+import List from '../shared/components/List'
+
+const Home = () => {
+  return (
+    <>
+      <Featured />
+      <List />
+    </>
+  )
+}
+
+export default Home
